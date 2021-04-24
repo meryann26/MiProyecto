8520958f3bf8dbf60d223382a6d4feccea968f67
50672 <50672@Mery-Ann>
2021-04-24 09:49:19 -0600

    Commit

--- src/decimocuartaclase/Usuario.java
+++ src/decimocuartaclase/Usuario.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package decimocuartaclase;
+import java.util.Date;
 
 /**
  *
@@ -15,8 +16,43 @@ public class Usuario {
     protected String loginStatus;
     protected Date registerDate;
     
+    public boolean verifyLogin(){
+    
+        return false;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
     
     
-            
     
 }
